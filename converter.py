79743315e866a4524cc1d5fd92b4ec3eff02a37a
lizekss <lekse20@freeuni.edu.ge>
2024-09-04 16:47:57 +0400

    separate app from converter

--- converter.py
+++ converter.py
@@ -1,5 +1,3 @@
-import tkinter as tk
-
 class CurrencyConverter:
     rates = {
         'USD': {'EUR': 0.85, 'GBP': 0.76, 'CAD': 1.27},
@@ -8,49 +6,9 @@ class CurrencyConverter:
         'CAD': {'USD': 0.79, 'EUR': 0.67, 'GBP': 0.59}
     }
 
+    def get_currencies(self):
+        return self.rates.keys()
+
     def convert(self, from_currency, to_currency, amount):
         rate = self.rates[from_currency][to_currency]
         return rate * amount
-
-FROM_DEFAULT = 'USD'
-TO_DEFAULT = 'EUR'
-
-def convert_currency():
-    c = CurrencyConverter()
-    from_currency = from_currency_var.get()
-    to_currency = to_currency_var.get()
-    amount = float(amount_field.get())
-    converted_amount = c.convert(from_currency, to_currency, amount)
-    converted_amount_var.set(round(converted_amount, 2))
-
-def clear_fields():
-    from_currency_var.set(FROM_DEFAULT)
-    to_currency_var.set(TO_DEFAULT)
-    amount_field.delete(0, tk.END)
-    converted_amount_var.set('')
-
-root = tk.Tk()
-
-from_currency_var = tk.StringVar(root)
-to_currency_var = tk.StringVar(root)
-amount_field = tk.Entry(root)
-converted_amount_var = tk.StringVar(root)
-
-from_currency_var.set(FROM_DEFAULT)
-to_currency_var.set(TO_DEFAULT)
-
-from_currency_option = tk.OptionMenu(root, from_currency_var, 'USD', 'EUR', 'GBP', 'CAD')
-to_currency_option = tk.OptionMenu(root, to_currency_var, 'USD', 'EUR', 'GBP', 'CAD')
-convert_button = tk.Button(root, text="Convert", command=convert_currency)
-clear_button = tk.Button(root, text="Clear", command=clear_fields)
-converted_amount_label = tk.Label(root, textvariable=converted_amount_var)
-
-amount_field.pack()
-from_currency_option.pack()
-to_currency_option.pack()
-convert_button.pack()
-clear_button.pack()
-converted_amount_label.pack()
-
-root.mainloop()
-
